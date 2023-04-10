db8fbc1faf6642a08a85d79194d7830c418a6a1e
Matias Di Iorio <matiasmd8610@gmail.com>
2023-04-10 15:33:13 -0300

    Updates

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -59,7 +59,7 @@ $(document).ready(function(){
       var target = this.hash,
       $target = $(target);
       $('html, body').stop().animate({
-          'scrollTop': $target.offset().top - 70
+          'scrollTop': $target.offset().top - 75
       }, 500, 'swing');
   });
 }); 
@@ -91,7 +91,6 @@ AOS.init({
   disableMutationObserver: false, // disables automatic mutations' detections (advanced)
   debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
   throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-  
 
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 120, // offset (in px) from the original trigger point
@@ -200,4 +199,13 @@ if (document.querySelector('.swiper-clientsLogos')) {
 //     });
 //   }
 // });
+
+
+/*HEADER SUBMENU MOBILE
+**********************************************/
+document.querySelectorAll('.has-submenu').forEach((element, index) => {
+  element.addEventListener('click', ()=> {
+    element.classList.toggle('active');
+  })
+});
 
